e243b6c525c51a32c572c7e41ae52892464d2522
semicircunferencia <125926149+semicircunferencia@users.noreply.github.com>
2023-03-25 22:44:57 +0100

    m

--- SistemaSolar.cpp
+++ SistemaSolar.cpp
@@ -149,6 +149,9 @@ void leercondiniciales(string nombre, double masas[], double posiciones[][2], do
             fichero >> velocidades[j][1];
         }
 
+        velocidades[5][0]=0.15;
+        velocidades[7][0]=0.07;
+
         fichero.close();
     }
 

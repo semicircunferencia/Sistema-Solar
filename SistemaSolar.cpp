991602dae09ed85c41409c8fdf2575f4071e0720
semicircunferencia <125926149+semicircunferencia@users.noreply.github.com>
2023-03-28 18:41:11 +0200

    m

--- SistemaSolar.cpp
+++ SistemaSolar.cpp
@@ -112,7 +112,7 @@ int main(void) {
                 datos << "\n";
 
                 // El fichero de Python
-                datospython << posiciones[i][0]-posiciones[3][0] << "," << posiciones[i][1]-posiciones[3][1] << "\n";
+                datospython << posiciones[i][0] << "," << posiciones[i][1] << "\n";
 
             }
 
